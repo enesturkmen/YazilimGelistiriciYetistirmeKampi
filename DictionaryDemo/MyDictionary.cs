6c35d7283a7889258ff3f99270f27d1dc388b425
Enes Türkmen <45971944+enesturkmen@users.noreply.github.com>
2021-02-13 17:01:31 +0300

    Odev 4.5

---
+++ DictionaryDemo/MyDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryDemo
+{
+    class MyDictionary<Key, Value>
+    {
+        Key[] keys;
+        Value[] values;
+        public MyDictionary()
+        {
+            keys = new Key[0];
+            values = new Value[0];
+        }
+
+        public void Add(Key key, Value value)
+        {
+            Key[] tempKey = keys;
+            Value[] tempValue = values;
+
+            keys = new Key[keys.Length + 1];
+            values = new Value[values.Length + 1];
+            int i;
+            int j;
+            for (i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+                for (j = 0; j < tempValue.Length; j++)
+                {
+                    values[j] = tempValue[j];
+                }
+                
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+    }
+}
---
+++ DictionaryDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+
+            myDictionary.Add(101, "Enes");
+            myDictionary.Add(102, "Mikail");
+            myDictionary.Add(103, "Ali Murat");
+
+        }
+    }
+}
