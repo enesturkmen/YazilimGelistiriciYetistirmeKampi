35f090e877a4a7d99384b7fae690f22b218b00b0
Enes Türkmen <45971944+enesturkmen@users.noreply.github.com>
2021-02-13 17:11:53 +0300

    odev 3.3

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Customer added: " + customer.Id + ", " + customer.Name + ", " + customer.Surname);
+        }
+        public void List(Customer customer)
+        {
+            Console.WriteLine("Customer listed: " + customer.Id + ", " + customer.Name + ", " + customer.Surname);
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Customer deleted: " + customer.Id + ", " + customer.Name + ", " + customer.Surname);
+        }
+    }
+}
--- ClassMethodDemo/Program.cs
+++ ClassMethodDemo/Program.cs
@@ -6,7 +6,16 @@ namespace ClassMethodDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Customer customer1 = new Customer();
+            CustomerManager customerManager = new CustomerManager();
+
+            customer1.Id = 1;
+            customer1.Name = "Enes";
+            customer1.Surname = "Turkmen";
+
+            customerManager.Add(customer1);
+            customerManager.List(customer1);
+            customerManager.Delete(customer1);
         }
     }
 }
