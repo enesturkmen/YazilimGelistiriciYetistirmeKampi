e127131de5301d32da8d5c0bb0b8fe0ce60c7363
Enes Türkmen <45971944+enesturkmen@users.noreply.github.com>
2021-02-28 22:05:42 +0300

    2802

--- ClassMethodDemo/Program.cs
+++ ClassMethodDemo/Program.cs
@@ -7,11 +7,23 @@ namespace ClassMethodDemo
         static void Main(string[] args)
         {
             Customer customer1 = new Customer();
+            Customer customer2 = new Customer();
             CustomerManager customerManager = new CustomerManager();
 
             customer1.Id = 1;
             customer1.Name = "Enes";
             customer1.Surname = "Turkmen";
+
+            customer2.Id = 2;
+            customer2.Name = "Mikail";
+            customer2.Surname = "Kaya";
+
+            Customer[] customers = new Customer[] { customer1, customer2 };
+
+            foreach (var allCustomers in customers)
+            {
+                Console.WriteLine(allCustomers.Id + " " + allCustomers.Name + " "+ allCustomers.Surname);
+            }
 
             customerManager.Add(customer1);
             customerManager.List(customer1);
